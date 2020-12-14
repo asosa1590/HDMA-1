4e36c7d0e238a0cc0341d27807713df3bcdbb932
asosa1590 <70542150+asosa1590@users.noreply.github.com>
2020-12-14 00:40:30 -0500

    Update Econometrics Project.R

--- Econometrics Project.R
+++ Econometrics Project.R
@@ -67,7 +67,17 @@ dfna$Preapproval_request<-ifelse(dfna$action_taken_name =='Preapproval request d
 dfna$Collateral<-ifelse(dfna$denial_reason_name_1 =='Collateral',1,0)
 dfna$Debt_to_income_ratio<-ifelse(dfna$denial_reason_name_1 =='Debt-to-income ratio',1,0)
 dfna$Credit_History<-ifelse(dfna$denial_reason_name_1=='Credit history ',1,0)
-dfna$Credit_app_incomplete<-ifelse(dfna$denial_reason_name_1 == 'Credit application incomplete',1,0)                             
+dfna$Credit_app_incomplete<-ifelse(dfna$denial_reason_name_1 == 'Credit application incomplete',1,0)
+
+# Co-Applicant Name
+dfna$Hispanic_or_Latino <- ifelse(dfna$co_applicant_ethnicity_name == 'Hispanic or Latino', 1, 0)
+dfna$Not_Hispanic_or_Latino<- ifelse(dfna$co_applicant_ethnicity_name == 'Not Hispanic or Latino', 1, 0)
+dfna$Asian <-ifelse(dfna$co_applicant_ethnicity_name =='Asian',1,0)
+dfna$Native_Hawaiian_or_P.Islander <-ifelse(dfna$co_applicant_ethnicity_name =='Native Hawaiian or Other Pacific Islander',1,0)
+dfna$Male<-ifelse(dfna$co_applicant_sex_name =='Male',1,0)
+dfna$Female<-ifelse(dfna$co_applicant_sex_name =='Female',1,0)
+
+
 
 summary(dfna$county_name)
 
